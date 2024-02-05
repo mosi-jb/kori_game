3f261d7a0f164d5058a203a2f871d3243fcb807a
moslem <hmtymslm50@gmail.com>
2024-02-06 02:51:13 +0330

    khodam nmidonam

--- games/serializers.py
+++ games/serializers.py
@@ -6,7 +6,7 @@ from games.models import Game, Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ('id', 'title', 'image', 'games')
 
 
 class GameSerializer(serializers.ModelSerializer):
@@ -15,3 +15,17 @@ class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = '__all__'
+
+
+class GameSerializer2(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = '__all__'
+
+
+class CategorySerializer2(serializers.ModelSerializer):
+    games = GameSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Category
+        fields = ('id', 'title', 'image', 'games')
